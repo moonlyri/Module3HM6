daa915edf704394c03e8b45882c2df9046d62288
Karina <3431645karina@gmail.com>
2022-10-26 20:43:10 +0300

    hm6 update

--- Module3HM6/Program.cs
+++ Module3HM6/Program.cs
@@ -5,20 +5,22 @@ class Program
     static void Main(string[] args)
     {
         var message = new MessageBox();
+        var taskcompletionsourse = new TaskCompletionSource<bool>();
         message.Open();
         message.action += state =>
         {
             if (state == State.Cansel)
             {
                 Console.WriteLine("Operation was canceled");
+                taskcompletionsourse.SetCanceled();
             }
             else if (state == State.Ok)
             {
                 Console.WriteLine("Operation was confirmed");
+                taskcompletionsourse.SetResult(true);
             }
 
-            return false;
+            return taskcompletionsourse.Task.GetAwaiter().GetResult();
         };
-
     }
 }
